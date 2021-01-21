8366174d6433b63706efe33329aff7b2cdf2c59e
Orawan\Orawan <614234021@parichat.skru.ac.th>
2021-01-21 11:07:35 +0700

    lab03

---
+++ src/components/BookItem.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="book-item">
+  <h1>{{book.title}}</h1>
+
+  <img v-bind:src="book.thumbnailUrl" /><br/>
+  <b>Author:</b> {{book.author}} <br/>
+  <b>Categoty:</b> {{book.category}} <br/>
+  <b>ISBN:</b> {{book.isbn}} <br/>
+  <b>Pages:</b> {{book.pageCount}} <br/>
+  <b>Description:</b><br/>
+  <p>{{book.shortDescription}}</p>  
+
+
+</div>
+    
+</template>
+
+
+
+<script>
+
+export default {
+  name: 'BookItem',
+  components: {
+    
+  },
+  props:["book"]
+
+}
+</script>
+
+
+
+
+<style>
+.book-item{
+    overflow: auto;
+    text-align: center;
+    background-color: antiquewhite;
+    padding: 10px;
+    border: 1px bisque;
+}
+
+
+</style>
---
+++ src/components/Books.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <div v-bind:key="book.bookid" v-for="book in book1">
+        <BookItem v-bind:book="book" />
+
+    </div>
+</div>
+    
+</template>
+
+
+
+<script>
+import BookItem from './BookItem.vue'
+export default {
+  name: 'Books',
+  components: {
+    BookItem,
+  },
+  props:["book1"]
+}
+</script>
+
+
+
+
+<style>
+
+</style>
